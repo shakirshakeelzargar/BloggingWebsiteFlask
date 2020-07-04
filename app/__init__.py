89e2d42611212dec9f09fb475a64fadd022e334a
ashir <ashir.provingit@gmail.com>
2020-07-04 16:58:00 +0530

    template path

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
-app = Flask(__name__)
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+app = Flask(__name__,template_folder=os.path.join(dir_path,"templates"))
+print(os.path.join(dir_path,"templates"))
 
 from app import routes
